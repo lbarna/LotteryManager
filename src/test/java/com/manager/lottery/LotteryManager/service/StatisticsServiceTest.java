9e0ad5119d3ac1fd834d108f36d71bb86624bc6d
Laszlo Barna <barnalaszlo89@gmail.com>
2017-10-14 10:52:38 +0200

    Lowered spring boot version to the latest release version and added a dummy unit test.

---
+++ src/test/java/com/manager/lottery/LotteryManager/service/StatisticsServiceTest.java
@@ -0,0 +1,18 @@
+package com.manager.lottery.LotteryManager.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatisticsServiceTest {
+
+    @Test
+    public void getMostFrequentNumbers() {
+        Assert.assertTrue(true);
+    }
+
+}
